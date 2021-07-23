b85fa221f9e4546919466ce1f1105379c0f3c0af
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2021-07-23 19:03:13 -0300

    Segunda troca

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -4,15 +4,26 @@ import Pokemon from './Pokemon';
 class Pokedex extends React.Component {
     constructor() {
         super();
+
+        this.state = {
+            atual: 0,
+        };
+
+        this.atualPokemon = this.atualPokemon.bind(this);
+    }
+
+    atualPokemon() {
+        if(this.state.atual < this.props.pokemons.length) 
+        this.setState({ atual: this.state.atual + 1 });
     }
 
     render() {
         return (
             <div className="pokedex">
 
-                <Pokemon key={ this.props.pokemons[0].id } pokemon={ this.props.pokemons[0] } />
+                <Pokemon key={ this.props.pokemons[0].id } pokemon={ this.props.pokemons[this.state.atual] } />
                 <div>
-                   <button>Enviar</button> 
+                   <button onClick={ this.atualPokemon }>Enviar</button> 
                 </div>
             </div>
         );
